942f2580e5fdcfabc32cce19c36849c71831ddcf
Zhong Zhenyu <nczzy1997@gmail.com>
2025-12-28 18:48:04 +0800

    Update build configs

--- build.lua
+++ build.lua
@@ -1,23 +1,10 @@
 module = "nkthesis"
 
 typesetexe = "xelatex"
+unpackexe = "xelatex"
+makeindexexe = "zhmakeindex"
+
 typesetopts = "-shell-escape -file-line-error -halt-on-error -interaction=nonstopmode -synctex=1"
-
-function typeset_special(name, path)
-  local errorlevel = 0
-  errorlevel = run(path, "zhmakeindex -s gind.ist " .. name .. ".idx")
-  if errorlevel ~= 0 then return errorlevel end
-  errorlevel = run(path, "zhmakeindex -s gglo.ist -o " .. name .. ".gls " .. name .. ".glo")
-  return errorlevel
-end
-
-cleanfiles = {"*.hd", "*.glo", "*.gls", "*.idx", "*.ind", "*.ilg"}
-
-typesetcmds = typesetcmds or ""
-
-sourcefiles = {"nkthesis.dtx", "nkthesis.ins"}
-
-unpackfiles = {"nkthesis.ins"}
 
 docfiles = {
   "frontmatter",
@@ -26,11 +13,7 @@ docfiles = {
   "nkthesis-sample.bib",
   "nkthesis-sample.tex",
 }
-
 typesetfiles = {"nkthesis.dtx", "nkthesis-sample.tex"}
 
-biberexe = "biber"
-
 checkengines = { "xetex" }
-
 checkruns = 3
