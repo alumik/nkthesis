5ffce8808ed87763f4b680d3e5bd154c7698cbb7
Zhong Zhenyu <nczzy1997@gmail.com>
2026-02-23 20:14:47 +0800

    docs: fix documentation inconsistencies and comments

--- build.lua
+++ build.lua
@@ -2,6 +2,7 @@ module = "nkthesis"
 
 typesetexe = "xelatex"
 unpackexe = "xelatex"
+-- 注意：zhmakeindex 需要单独安装，请参考 README.md 中的说明
 makeindexexe = "zhmakeindex"
 
 typesetopts = "-shell-escape -file-line-error -halt-on-error -interaction=nonstopmode -synctex=1"
